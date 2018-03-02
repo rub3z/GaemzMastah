936ad70b6de66cc836a557381488bc5c7a58f19d
Sotheanith Sok <thetheanith@gmail.com>
2018-03-02 00:59:24 -0800

    add Launcher

--- src/launcher/controller/Main.java
+++ src/launcher/controller/Main.java
@@ -1,15 +1,29 @@
 package launcher.controller;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import launcher.model.ProgramManager;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
+        new ProgramManager("src/launcher/resources/Database.xml").readDataFromFile();
         launch(args);
     }
+
     @Override
     public void start(Stage primaryStage) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/launcher/view/MainView.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setMaximized(true);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/launcher/controller/MainViewController.java
+++ src/launcher/controller/MainViewController.java
@@ -1,4 +1,27 @@
 package launcher.controller;
 
+import javafx.fxml.FXML;
+import launcher.model.Program;
+import launcher.model.ProgramManager;
+
+import java.util.ArrayList;
+
 public class MainViewController {
+    @FXML
+    TopLogoController topLogoController;
+    @FXML
+    BottomLogoController bottomLogoController;
+    @FXML
+    GameListController gameListController;
+    ProgramManager programManager;
+
+    public MainViewController() {
+        programManager = new ProgramManager("src/launcher/resources/Database.xml");
+    }
+
+    @FXML
+    public void initialize() {
+        gameListController.initializeParentController(this);
+        gameListController.addAppsToView(new ArrayList<Program>(programManager.getAppList()));
+    }
 }
